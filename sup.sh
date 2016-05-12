dfd610f943924eaa5d5863a6659c52f3d42284d9
leandrodeleite <leandrodeleite@gmail.com>
2016-05-12 18:05:09 -0300

    Comment apt/up* script

--- sup.sh
+++ sup.sh
@@ -6,3 +6,10 @@ sudo \
   apt-get upgrade -y \
 \
 
+
+# update       - check what is new, read existing list/entries in repo
+# upgrade      - all updates for installed packages WITHOUT kernel modules
+# dist-upgrade - all updates for installed packages WITH    kernel modules
+#   no release update
+#   -s - test only, no changes
+
