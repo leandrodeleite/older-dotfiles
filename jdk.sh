f90dfe3c963d19c66b6e592b6fbd3fec19ffb91a
leandrodeleite <leandrodeleite@gmail.com>
2016-05-12 21:30:01 -0300

    Create jdk check/installation script

---
+++ jdk.sh
@@ -0,0 +1,14 @@
+# list if installed already
+dpkg --list | grep -i jdk
+
+java  -version
+
+javac -version
+
+sudo \
+  apt-get install \
+    openjdk-8-jdk
+
+#   openjdk-8-jre
+#   for browsers/plugin
+
