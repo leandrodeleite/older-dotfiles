576b5b41b46689e8b29e3690967d6f006403d544
leandrodeleite <leandrodeleite@gmail.com>
2016-05-12 22:09:05 -0300

    Create JVM/config script

---
+++ jvm.sh
@@ -0,0 +1,18 @@
+# manage multiple JVM installations
+# java
+# javac
+
+sudo \
+  update-alternatives --config java
+
+sudo \
+  update-alternatives --config javac
+
+
+echo $JAVA_HOME
+
+# /etc/environment
+#
+# JAVA_HOME="/path/to/jvm"
+
+
