fb5c8993c684a50a55fdce2efa2a6db82c49c303
leandrodeleite <leandrodeleite@gmail.com>
2016-05-12 21:46:09 -0300

    Create SDKMAN script

---
+++ sdkman.sh
@@ -0,0 +1,21 @@
+# requires
+sudo \
+  apt-get install \
+    curl \
+    unzip \
+  \
+# depends ?
+# jre/jdk
+#
+#
+# SDKMAN.io
+# -s - silent <- not good option
+
+curl get.sdkman.io | bash 
+
+# After
+# source "$HOME/.sdkman/bin/sdkman-init.sh"
+# Then
+# source help
+# source version
+
